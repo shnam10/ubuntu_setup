eb80cd4aa2e9e0a390d00fe118ee7496bcc7d659
Seongho Nam <indnd10@gmail.com>
2023-11-20 19:30:44 +0900

    Create slam_dev_install.sh

---
+++ slam_dev_install.sh
@@ -0,0 +1,11 @@
+
+# Ceres
+sudo apt-get install -y cmake libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev
+CERES_VERSION="2.0.0"
+git clone https://ceres-solver.googlesource.com/ceres-solver
+cd ceres-solver
+git checkout tags/${CERES_VERSION}
+mkdir build && cd build
+cmake ..
+make
+sudo make install
