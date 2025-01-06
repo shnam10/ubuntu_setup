ceb1d1cae30c27459a4455ef308f63714e1d4aef
Seongho Nam <indnd10@gmail.com>
2025-01-06 11:14:12 +0900

    Update slam_dev_install.sh
    manif & smooth

--- slam_dev_install.sh
+++ slam_dev_install.sh
@@ -17,4 +17,20 @@ make
 # Sophus
 git clone https://github.com/strasdat/Sophus
 
+# manif
+git clone https://github.com/artivis/manif.git
+cd manif && mkdir build && cd build
+cmake ..
+make install
+
+# smooth
+git clone https://github.com/pettni/smooth.git
+cd smooth
+mkdir build && cd build
+# Specify a C++20-compatible compiler if your default does not support C++20.
+# Build tests and/or examples as desired.
+cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF
+make -j8
+sudo make install
+
 # TODO: Pangolin 
